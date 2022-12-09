903196d71f2f073408b9ca15df2956e029effee9
Melinna <Melinna.art@gmail.com>
2022-12-09 14:38:27 -0500

    add packagesand start setting up DB

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const PORT = 1337
 
 require('dotenv').config()
 
+
+const uri = DB_STRING;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    console.log('DB connected')
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+
 app.listen('/', PORT => {
     console.log(`Your server on port ${PORT} is running, better go catch it!`)
 })
