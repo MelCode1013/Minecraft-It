65f3af9d115a202a8e652ec35510ed279104fa44
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-01-04 14:19:00 -0500

    add console.log to login route to make sure it is running

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get("/login", (req,res) => {
+    console.log('router is workin!')
     res.render('login', {
         pageTitle: "Log in"
     })
