91617d84e35206a62bcce2411a5be485ae14ad8b
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-01-03 21:18:19 -0500

    create routes directory and add js for login route

---
+++ routes/loginRoute.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get("login", (req,res) => {
+    res.render('login', {
+        pageTitle: "Log in"
+    })
+})
+
+module.exports = router
