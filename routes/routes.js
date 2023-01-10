5f2068426484bf410e097df259f8b65cdf67647f
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-01-10 14:16:42 -0500

    add upload build button

--- routes/routes.js
+++ routes/routes.js
@@ -9,4 +9,19 @@ router.get("/feed", (req,res) => {
     })
 })
 
+router.post("/feed", (req,res) => {
+    console.log('feed router is workin!')
+    res.render('feed', {
+        pageTitle: "Dashboard"
+    })
+})
+
+router.get("/post", (req,res) => {
+    console.log('post item router is workin!')
+    res.render('post', {
+        pageTitle: "Add your build"
+    })
+})
+
+
 module.exports = router
