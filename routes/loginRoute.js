43306da70420a4a56c3d269a4fb563d267b170d8
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-01-19 20:31:42 -0500

    update login and sign up routes

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const {registerView, loginView } = require('../controllers/loginController');
+router.get('/register', registerView);
+router.get('/login', loginView);
 
 router.get("/login", (req,res) => {
     console.log('router is workin!')
-    res.render('login', {
-        pageTitle: "Log in"
-    })
+    
 })
 
 module.exports = router
