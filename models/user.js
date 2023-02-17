06056c10aafe4bf5e658d189f08d503384810f2f
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-02-16 19:34:55 -0500

    create user schema

---
+++ models/user.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+   userName: { type: String, unique: true },
+   email: { type: String, unique: true },
+   password: String,
+})
